9741a07328b36f3822f1fed2c5ee274d776198e4
Manali Deb <Debook4@gmail.com>
2024-09-23 22:29:22 -0500

    syntax cleanup

--- src/main/Problem 1.py
+++ src/main/Problem 1.py
@@ -71,5 +71,5 @@ plt.show()
 # measure and tell accuracy
 prediction = np.array([model.predict(x) for x in X])
 accuracy = np.mean(prediction == y)
-print(f"Accuracy of training set: {accuracy * 100:.2f}f")
+print(f"Accuracy of training set: {accuracy * 100:.2f}%")
 
